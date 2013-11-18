fca7322f089de9e7e5506602ec24fc2504483eeb
vagrant <vagrant@precise64.(none)>
2013-11-18 18:51:04 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsGeocoder::Application.config.secret_token = 'd07498d9319e2558bbcf0951fac7de31e12dac094d466a98dadc884a839f8006b83872df9c09ee2323fcf9c2ca345289a2ffb78f0de9d9f914bb84c892b4b0c3'
